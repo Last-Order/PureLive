39f33ae5d68d57c9f40d5915daa6f65364bbbbe5
Eridanus Sora <Last-Order@users.noreply.github.com>
2018-01-14 22:53:48 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name New Script
+// @namespace Violentmonkey Scripts
+// @include https://www.zhanqi.tv/*    
+// @grant none
+// ==/UserScript==
+$(document).ready(() => {
+  const zhanqi = () => {
+     $('.live-chat-content').remove(); // 聊天窗口
+    $('.live-side-content').remove(); // 左侧边栏
+    $('.want-headlines-btn').remove(); // 上头条
+    $('.js-room-fun-area').remove(); // 礼物区域
+    $('.sdk-zhanqiMall').remove(); // 商城按钮
+    let styleTag = $('<style>\
+    .no-play-down-ibox {display: none !important;} /* 客户端下载广告 */\
+    #js-flash-layer {width: 100vw !important; height: 100vh !important;} /* 拉伸播放器 */\
+    .live-room-content {padding: 0 !important;} /* 砍掉主容器左内间距 */\
+    .sdk-zhanqiMall {display: none !important;} /* 商城按钮 */\
+    </style>')
+    $('html > head').append(styleTag);
+  }
+  
+  switch(location.host){
+    case 'www.zhanqi.tv':
+    case 'zhanqi.tv':
+      zhanqi(); break;
+  }
+});
